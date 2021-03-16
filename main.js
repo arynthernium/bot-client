2eccfbbf64ef3814dd71a72fab7078ebe89a4d8f
arynthernium <69891576+arynthernium@users.noreply.github.com>
2021-03-16 12:47:54 -0400

    added css

---
+++ main.js
@@ -0,0 +1,31 @@
+const { app, BrowserWindow } = require('electron')
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+function createWindow () {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false
+    }
+  })
+
+  win.loadFile('index.html');
+  // win.removeMenu();
+}
+
+app.whenReady().then(createWindow)
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow()
+  }
+})
